50578e37bc70ec0ac40a101f1a1a33c6c93b72f7
aiswalprince07 <81444348+aiswalprince07@users.noreply.github.com>
2024-07-16 23:49:53 +0530

    add multer middleware and cloudinary in utils

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from "multer"
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {  // ye file multer me mil jta , cb --> callback
+        cb(null, "./public/te   mp")
+    },
+    filename: function (req, file, cb) {
+        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)  // ye file ka name me prefix add karne ke liye use !!
+        // cb(null, file.fieldname + '-' + uniqueSuffix)
+        cb(null, file.originalname)
+    }
+})
+
+export const upload = multer({ 
+    storage,
+})
